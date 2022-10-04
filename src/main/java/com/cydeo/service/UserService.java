cb9ad8e0e54896878d4735f4931941dfdd27da18
saityagci <saityagci@gmail.com>
2022-10-04 19:35:20 -0400

    c

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -5,9 +5,5 @@ import com.cydeo.dto.UserDTO;
 import java.util.List;
 
 public interface UserService extends CrudService<UserDTO,String>{
-    UserDTO save(UserDTO userDTO);
-    UserDTO findById(String userName);
-    List<UserDTO> findAll();
-    void delete(UserDTO user);
-    void deleteById(String userName);
+
 }
