94942f28ecc2076f439fde07829794ba0313df21
saityagci <saityagci@gmail.com>
2022-10-04 23:38:35 -0400

    Data generator component

--- src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,14 +1,24 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.RoleService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataGenerator implements CommandLineRunner {
+    RoleService roleService;
+
+    public DataGenerator(RoleService roleService) {
+        this.roleService = roleService;
+    }
 
     @Override
     public void run(String... args) throws Exception {
         RoleDTO adminRole=new RoleDTO(1L,"Admin");
         RoleDTO managerRole=new RoleDTO(1L,"Manager");
         RoleDTO employeeRole=new RoleDTO(1L,"Employee");
+        // add data to MAP
+
     }
 }
